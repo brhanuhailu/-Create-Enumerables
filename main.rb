7d44f28b30ed64df61ff908abbc8168ced19416f
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-11 03:23:55 -0700

    main rubocop

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
 require_relative 'enumerable'
 
 class MyList
-include MyEnumerable
-    def initialize(*data)
-        @list = data
-    end
+  include MyEnumerable
+  def initialize(*data)
+    @list = data
+  end
 
-    def each(&block)
-        @list.each(&block)
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
-list = MyList.new(1,2,3,4,5,6,7)
+list = MyList.new(1, 2, 3, 4, 5, 6, 7)
 
-p(list.all? {|e| e < 8})
-p(list.all? {|e| e > 8})
-p(list.any? {|e| e == 2})
-p(list.any? {|e| e == 5})
-p(list.filter &:even?)
-p(list.filter &:odd?)
-p(list.any?{|e| e == 3})
+p(list.all? { |e| e < 8 })
+p(list.all? { |e| e > 8 })
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
+p(list.filter(&:even?))
+p(list.filter(&:odd?))
+p(list.any? { |e| e == 3 })
